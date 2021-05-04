d3ad0f8cc63b0373b8827d5848645d87389d3ac0
Petter55 <Edison_Petter@Dell.com>
2021-05-03 23:54:29 -0300

    created search tabs components; finished search by subject

--- src/commons/http-constants.ts
+++ src/commons/http-constants.ts
@@ -1,11 +1,11 @@
-//export const baseUrls = {
-//  applicationServiceUrl: "https://chat-application-service.herokuapp.com",
-//    applicationManagerUrl: "https://chat-application-manager.herokuapp.com"
-//}
 export const baseUrls = {
-    applicationServiceUrl: "http://localhost:7011",
-      applicationManagerUrl: "http://localhost:5000"
-  }
+  applicationServiceUrl: "https://chat-application-service.herokuapp.com",
+    applicationManagerUrl: "https://chat-application-manager.herokuapp.com"
+}
+//export const baseUrls = {
+//    applicationServiceUrl: "http://localhost:7011",
+//      applicationManagerUrl: "http://localhost:5000"
+//  }
 
 export const defaultHeader = {
   'Accept': 'application/json',
--- src/components/app/home/join-container/JoinContainer.tsx
+++ src/components/app/home/join-container/JoinContainer.tsx
@@ -3,25 +3,9 @@ import React, { SyntheticEvent, useEffect, useState } from 'react';
 import './JoinContainer.css';
 import {Tab, TabList, TabPanel, Tabs} from 'react-tabs'
 import 'react-tabs/style/react-tabs.css';
-import { baseUrls, defaultHeader } from '../../../../commons/http-constants';
-import Modal from 'react-modal';
-import Conversation from '../../../../models/Conversation';
-const closeButton = require('../../../../assets/close.svg').default
+import SearchBySubjectTab from './Search-by-subject-tab/SearchBySubjectTab';
+import SearchByLinkTab from './Search-by-link-tab/SearchByLinkTab';
 
-const customStyles = {
-    content : {
-        margin: "auto",
-        backgroundColor:"rgb(149, 105, 153)",
-        borderRadius: 10,
-        border: 0,
-        padding: "0px 20px 30px 20px",
-        top: 40,
-        left: 40,
-        minHeight: "10%",
-        maxHeight: "40%",
-        fontFamily: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif"
-    }
-  }
 
 interface JoinContainerProps {
     joinConversationByLink: Function
@@ -34,52 +18,10 @@ interface JoinContainerForm {
 
 function JoinContainer(props: any & JoinContainerProps) {
     const [dropdown, setDropdown] = useState<boolean>(false)
-    const [modalIsOpen,setIsOpen] = React.useState(false)
-
-    const [formValue, setFormValue] = useState<JoinContainerForm>({
-        linkInput: "",
-        searchInput: ""
-    })
-
-    const [ conversationsSearched, setConversationsSearched ] = useState<Array<Conversation>>([])
-
+    
     const expandForm = () => {
         setDropdown(!dropdown)
     }
-
-
-    const handleChange = (event: any) => {
-        setFormValue({ ...formValue, [event.currentTarget.name]: event.currentTarget.value })
-    };
-
-
-    const submitJoinByLink = (event: SyntheticEvent) => {
-        event.preventDefault()
-        props.joinConversationByLink(formValue.linkInput)
-    }
-
-    const submitSearchBySubject = async (event: SyntheticEvent) => {
-        event.preventDefault()
-        openModal()
-        const unparsedConvs= await fetch(`${baseUrls.applicationServiceUrl}/conversation/subject/${formValue.searchInput}`,
-        { method: 'GET', headers: defaultHeader })
-        const conversations:Array<Conversation> = await unparsedConvs.json()
-        console.log(conversations);
-        
-        setConversationsSearched(conversations)
-    }
-
-    function openModal() {
-        setIsOpen(true);
-      }
-    
-      function afterOpenModal() {
-        // references are now sync'd and can be accessed.
-      }
-    
-      function closeModal(){
-        setIsOpen(false);
-      }
 
 
     return (
@@ -93,50 +35,14 @@ function JoinContainer(props: any & JoinContainerProps) {
                         <Tab style={{backgroundColor: "transparent", color:"white"}}>Por assunto</Tab>
                     </TabList>
                     <TabPanel>
-                        <form onSubmit={submitJoinByLink}>
-                            <div>
-                                <div className="form-container">
-                                    <input className="join-input-field" name="linkInput" value={formValue.linkInput} onChange={(e) => handleChange(e)} id="conversationHash" type="text" />
-                                    <button className="go-button" type="submit">Ir</button>
-                                </div>
-                            </div>
-                        </form>
+                        <SearchByLinkTab joinConversationByLink={props.joinConversationByLink}/>
                     </TabPanel>
                     <TabPanel>
-                        <form onSubmit={submitSearchBySubject}>
-                            <div>
-                                <div className="form-container">
-                                    <input className="join-input-field" name="searchInput" value={formValue.searchInput} onChange={(e) => handleChange(e)} id="searchInput" type="text" />
-                                    <button className="go-button" type="submit">Procurar</button>
-                                </div>
-                            </div>
-                        </form>
+                        <SearchBySubjectTab joinConversationByLink={props.joinConversationByLink}/>
                     </TabPanel>
                     </Tabs>
                 </div>
                 : <></>}
-                <Modal
-          isOpen={modalIsOpen}
-          onAfterOpen={afterOpenModal}
-          onRequestClose={closeModal}
-          contentLabel="Search"
-          style={customStyles}
-        >
-          
-          <div>
-          <div className="closing-modal">
-              <img className="close-btn" alt="x" onClick={closeModal} src={closeButton}/>
-          </div>
-          <p>{conversationsSearched.length} {conversationsSearched.length>1 ? "resultados" : "resultado" }</p>
-            <div>
-                {conversationsSearched && conversationsSearched.length > 0 ?
-                conversationsSearched.map((element, index)=>(
-                    <div className="conversation-found" key={index}>{element?.subject}</div>
-                )) 
-                : <></>}
-            </div>
-            </div>
-        </Modal>
         </div>
     )
 }
